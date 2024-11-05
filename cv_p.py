250c05dff14be64997dfa65b724d116309593cbf
Taneeshk Patel <69989113+taneeshk12@users.noreply.github.com>
2024-11-05 11:42:32 +0530

    Update cv_p.py

--- cv_p.py
+++ cv_p.py
@@ -39,16 +39,16 @@ from firebase_admin import credentials
 # Initialize Firebase app if it hasn't been initialized yet
 if not firebase_admin._apps:
     cred = credentials.Certificate({
-        "type": st.secrets["TYPE"],
-        "project_id": st.secrets["PROJECT_ID"],
-        "private_key_id": st.secrets["PRIVATE_KEY_ID"],
-        "private_key": st.secrets["PRIVATE_KEY"].replace("\\n", "\n"),
-        "client_email": st.secrets["CLIENT_EMAIL"],
-        "client_id": st.secrets["CLIENT_ID"],
-        "auth_uri": st.secrets["AUTH_URI"],
-        "token_uri": st.secrets["TOKEN_URI"],
-        "auth_provider_x509_cert_url": st.secrets["AUTH_PROVIDER_X509_CERT_URL"],
-        "client_x509_cert_url": st.secrets["CLIENT_X509_CERT_URL"]
+        "type": st.secrets["type"],
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["private_key_id"],
+        "private_key": st.secrets["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"]
     })
     firebase_admin.initialize_app(cred)
 
