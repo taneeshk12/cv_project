69a039074adbaf3f8a44c559917de924d7b1cb3a
Taneeshk Patel <69989113+taneeshk12@users.noreply.github.com>
2024-11-05 15:21:05 +0530

    Update cv_p.py

--- cv_p.py
+++ cv_p.py
@@ -230,16 +230,21 @@ from dotenv import load_dotenv
 import os
 import json
 
-# Initialize Firebase
-load_dotenv()
-
-firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
-if firebase_credentials:
-    cred = credentials.Certificate(json.loads(firebase_credentials))
-    if not firebase_admin._apps:
-        initialize_app(cred)
-else:
-    print("Error: Firebase credentials not found in .env")
+# Initialize Firebase app if it hasn't been initialized yet
+if not firebase_admin._apps:
+    cred = credentials.Certificate({
+        "type": st.secrets["type"],
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["private_key_id"],
+        "private_key": st.secrets["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+    })
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
